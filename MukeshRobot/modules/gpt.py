1d151a9f0957d73e496cb0e871356faf69898d6d
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2023-12-01 17:24:51 +0530

    Update gpt.py

--- MukeshRobot/modules/gpt.py
+++ MukeshRobot/modules/gpt.py
@@ -12,19 +12,23 @@ async def chat_gpt(event):
 
     query = event.pattern_match.group(1)
 
-    params = {"message": query}
-    response = requests.post(GPT_API_URL, data=params)
+    if query:
+        # Make a request to GPT API
+        response = requests.get(f"{GPT_API_URL}/?question={query}")
 
-    if response.status_code == 200:
-        result = response.json()
+        if response.status_code == 200:
+            # Extract the answer from the API response
+            result = response.json()
 
-        # Check if "join" key is present and remove it
-        if "join" in result:
-            del result["join"]
+            # Check if "join" key is present and remove it
+            if "join" in result:
+                del result["join"]
 
-        reply_message = result.get("message", "No response from ChatGPT")
+            reply_message = result.get("answer", "No answer received from ChatGPT.")
+        else:
+            reply_message = "Error communicating with ChatGPT API."
     else:
-        reply_message = "Error communicating with ChatGPT API"
+        reply_message = "Please provide a question after /gpt command."
 
     await event.reply(reply_message)
 
